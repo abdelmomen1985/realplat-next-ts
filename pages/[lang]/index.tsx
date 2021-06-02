6d4880f927286d7003b1e9c921b436c26062717f
Momen <abdelmomen1985@gmail.com>
2021-06-02 10:04:01 +0200

    fetch hubgraph

--- pages/[lang]/index.tsx
+++ pages/[lang]/index.tsx
@@ -1,17 +1,22 @@
-import { useEffect, useState } from 'react';
-import Layout from '../../components/Layouts/Layout';
-import { GetStaticPaths, GetStaticProps, NextPage } from 'next';
-import { getLocalizationProps } from '../../Context/LangContext';
+import { useEffect } from "react";
+import Layout from "../../components/Layouts/Layout";
+import { GetStaticPaths, GetStaticProps, NextPage } from "next";
+import { getLocalizationProps } from "../../Context/LangContext";
 // import useTranslation from '../../hooks/useTranslation';
-import MainSection from './../../components/HomeSections/MainSection';
-import ServicesSection from './../../components/HomeSections/ServicesSection';
-import ExplorerSection from './../../components/HomeSections/ExplorerSection';
-import GuidesSection from './../../components/HomeSections/GuidesSection';
+import MainSection from "./../../components/HomeSections/MainSection";
+import ServicesSection from "./../../components/HomeSections/ServicesSection";
+import ExplorerSection from "./../../components/HomeSections/ExplorerSection";
+import GuidesSection from "./../../components/HomeSections/GuidesSection";
 
 const IndexPage: NextPage = () => {
   // const { t } = useTranslation();
 
-
+  // just refresh the heroku api
+  useEffect(() => {
+    fetch("https://hubgraph.herokuapp.com/").then(() => {
+      console.log("hubgraph fetched");
+    });
+  }, []);
   return (
     <Layout title="Realestate Brand">
       {/* top section */}
@@ -27,7 +32,7 @@ const IndexPage: NextPage = () => {
 };
 
 export const getStaticProps: GetStaticProps = async (ctx) => {
-  const localization = getLocalizationProps(ctx, 'common');
+  const localization = getLocalizationProps(ctx, "common");
   return {
     props: {
       localization,
@@ -37,7 +42,7 @@ export const getStaticProps: GetStaticProps = async (ctx) => {
 
 export const getStaticPaths: GetStaticPaths = async () => {
   return {
-    paths: ['en', 'ar'].map((lang) => ({ params: { lang } })),
+    paths: ["en", "ar"].map((lang) => ({ params: { lang } })),
     fallback: false,
   };
 };
