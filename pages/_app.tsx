b327bf47fa1afc1adc8161b07a371cfaed5f3708
Momen <abdelmomen1985@gmail.com>
2021-07-29 13:42:06 +0200

    to remove

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,20 @@
-import "../styles/index.css";
 import { ApolloProvider } from "@apollo/client";
-import { useApollo } from "../lib/apolloClient";
-import { LanguageProvider } from "../Context/LangContext";
 import "@fortawesome/fontawesome-free/css/all.css";
+import React from "react";
+import "react-day-picker/lib/style.css";
+import "react-super-responsive-table/dist/SuperResponsiveTableStyle.css";
 import { AppContextProvider } from "../Context/AppContextProvider";
-import 'react-day-picker/lib/style.css';
-import 'react-super-responsive-table/dist/SuperResponsiveTableStyle.css';
+import { LanguageProvider } from "../Context/LangContext";
+import { getInitialLocale } from "../i18n/getInitialLocale";
+import { useApollo } from "../lib/apolloClient";
+import "../styles/index.css";
 
 export default function MyApp(ctx: any) {
+  // TODO remove
+  React.useEffect(() => {
+    window.location.replace(`/${getInitialLocale()}`);
+    //window.location.replace(`/english`);
+  });
   const apolloClient = useApollo(ctx.pageProps.initialApolloState);
   return (
     <ApolloProvider client={apolloClient}>
