a3abdb9ac86bbba95c4c4d67e9dd1681d76409e6
Momen <abdelmomen1985@gmail.com>
2021-08-02 21:47:50 +0200

    rework

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,16 +5,10 @@ import "react-day-picker/lib/style.css";
 import "react-super-responsive-table/dist/SuperResponsiveTableStyle.css";
 import { AppContextProvider } from "../Context/AppContextProvider";
 import { LanguageProvider } from "../Context/LangContext";
-import { getInitialLocale } from "../i18n/getInitialLocale";
 import { useApollo } from "../lib/apolloClient";
 import "../styles/index.css";
 
 export default function MyApp(ctx: any) {
-  // TODO remove
-  React.useEffect(() => {
-    window.location.replace(`/${getInitialLocale()}`);
-    //window.location.replace(`/english`);
-  });
   const apolloClient = useApollo(ctx.pageProps.initialApolloState);
   return (
     <ApolloProvider client={apolloClient}>
